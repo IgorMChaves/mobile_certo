58bea0e7ea8a2587ed5a67a1be19389822c6ad4a
IgorMChaves <igor.1904.chaves@gmail.com>
2022-06-21 11:56:02 -0300

    loginzin

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,22 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
-  View,
-  Text,
-  KeyboardAvoidingView,
-  TextInput,
+ View,
+ Text,
+ TextInput,
+ KeyboardAvoidingView,
+ Alert,
 } from "react-native";
 import { MaterialIcons, Entypo } from "@expo/vector-icons";
-import Button from "../../components/Button";
+import { Button } from "../../components";
 import styles from "./styles";
 import { LoginTypes } from "../../types/Screen.types";
+import { useAuth } from "../../hook/auth";
+import { IAuthenticate, IUser } from "../../interfaces/User.interface";
+import { AxiosError } from "axios";
 
 export default function Login({ navigation }: LoginTypes) {
-  async function handleSignIn() {
-    navigation.navigate("Tab");
-  }
+  const { signIn } = useAuth();
+  const [data, setData] = useState<IAuthenticate>();
+  const [isLoading, setIsLoading] = useState(true);
   function handleCadastrar() {
     navigation.navigate("Cadastrar");
   }
+  function handleChange(item: IAuthenticate) {
+    setData({ ...data, ...item });
+  }
+  async function handleSignIn() {
+  try {
+    setIsLoading(true);
+    if (data?.email && data.password) {
+      await signIn(data);
+    } else {
+      Alert.alert("Preencha todos os campos!!!");
+    setIsLoading(false);
+    }
+  } catch (error) {
+    const err = error as AxiosError;
+    const data = err.response?.data as IUser;
+    let message = "";
+    if (data.data) {
+      for (const [key, value] of Object.entries(data.data)) {
+        message = `${message} ${value}`;
+      }
+    }
+    Alert.alert(`${data.message} ${message}`);
+    setIsLoading(false);
+ }
+ }
+ 
+ useEffect(() => {
+  setIsLoading(false);
+ }, []);
 
   return (
     <View style={styles.container}>
